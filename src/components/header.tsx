7abae97c5eda8b3710e02e7c07d763e301f36777
Nathally <nathallyccd@gmail.com>
2024-04-02 17:54:06 -0300

    First commit

---
+++ src/components/header.tsx
@@ -0,0 +1,14 @@
+import nlwIcon from '../assets/nlw-unite-icon.svg';
+
+export function Header() {
+  return (
+    <header className='flex items-center gap-5 py-2'>
+      <img src={nlwIcon} />
+
+      <nav className='flex items-center gap-5'>
+        <a href="#" className='font-medium text-sm text-zinc-300'>Eventos</a>
+        <a href="#" className='font-medium text-sm'>Participantes</a>
+      </nav>
+    </header>
+  );
+}
